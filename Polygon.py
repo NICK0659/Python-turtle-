385955b97b2a33c720b96a6af475f6fb55ace48e
Nikhilesh Gautam <nikhileshgautam69@gmail.com>
2022-10-25 09:58:20 +0530

    added heart file

--- Polygon.py
+++ Polygon.py
@@ -2,13 +2,13 @@ import turtle as t
 import time
 print("enter the number of sides")
 n=int(input())
-t.bgcolor("black")
+t.bgcolor("yellow")
 t.title("My First Graphic")
 t.speed(1)
-t.color("white")
 poly_=t.Turtle()
 for i in range(n):
-    poly_.color("white")
+    poly_.shape("turtle")
+    poly_.color("red","yellow")
     poly_.forward(100)
     poly_.left(360/n)
 t.done()
